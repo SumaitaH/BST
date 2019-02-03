79e27b83fbd477fe25199639377e6068ccf5e879
Sumaita <sumaitah5@gmail.com>
2019-02-03 18:20:58 -0500

    First commit

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,22 @@
+class BinarySearchTree{
+	
+	//variables 
+	//1. node instance 
+	
+	node tree; 
+	
+	//functions 
+	//1. get current node value
+	//2. insert elements into BST
+	//3. remove elements into BST
+	//4. flip search tree 
+	//5. get leftmost value
+	//6. get rightmost value
+	
+	BinarySearchTree(int value){
+		tree = new node(value);
+	}
+	
+	
+	
+}
---
+++ src/node.java
@@ -0,0 +1,11 @@
+public class node{
+	
+	private int value; 
+	private node leftNode; 
+	private node rightNode; 
+	
+	node(int val){
+		this.value = val; 
+	}
+	
+}
