62946057cb553125d679f682a59987c65edfc90c
Sumaita <sumaitah5@gmail.com>
2019-02-03 19:24:00 -0500

    updated function

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -37,6 +37,30 @@ class BinarySearchTree{
 					}
 			}
 	}
+	//get leftmost value
+	public int minValue(node n) {
+		while(n.leftNode!=null) {
+			n=n.leftNode; 
+		}
+		return n.value; 
+	}
+	//get rightmost value
+	public int maxValue(node n) {
+		while(n.rightNode!=null) {
+			n=n.rightNode; 
+		}
+		return n.value; 
+	}
+	//inorder traversal 
+	public void inorderTraversal(node n) {
+		if(n.leftNode!=null) {
+			inorderTraversal(n.leftNode);
+		}
+		System.out.println(n.value);
+		if(n.rightNode!=null) {
+			inorderTraversal(n.rightNode);
+		}
+	}
 	public static void main(String[] args) {
 		
 		BinarySearchTree test = new BinarySearchTree(1);
@@ -44,7 +68,12 @@ class BinarySearchTree{
 		test.insert(5, test.tree);
 		test.insert(3, test.tree);
 		
-		System.out.println(test.tree.rightNode.value);
+		test.inorderTraversal(test.tree);
+		//System.out.println(test.tree.rightNode.value);
+		//System.out.println(test.minValue(test.tree));
+		//System.out.println(test.maxValue(test.tree));
+		
+		
 	}
 	
 }
